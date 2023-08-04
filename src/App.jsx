299cdf7807661402142c8d29fac16cc8696ee1d5
Ikbal101 <islamlover000001@gmail.com>
2023-08-04 19:33:08 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ function App() {
   return (
     <>
       <h1 className="text-center text-3xl font-bold">Hello world!</h1>
+      <h1 className="text-center text-3xl font-bold">Ikbal</h1>
     </>
   );
 }
