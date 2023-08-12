1fd947504d8621ac9fa06931401fbb8d355474b1
Prantosh <bepariprantosh360@gmail.com>
2023-08-12 17:50:00 +0600

    change style

--- src/Pages/Tours/Tours.jsx
+++ src/Pages/Tours/Tours.jsx
@@ -3,7 +3,7 @@ import TravelPackages from "../Home/TravelPackages/TravelPackages";
 
 const Tours = () => {
   return (
-    <div className="w-11/12 mx-auto sm:w-11/12 md:w-10/12 lg:w-9/12">
+    <div className="w-11/12 mx-auto sm:w-11/12 md:w-10/12 lg:w-9/12 -z-100">
       <TravelPackages />
     </div>
   );
