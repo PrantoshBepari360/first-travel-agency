9f09586385ce954504473635b5721ca61d5a9757
proshantosaha <proshantoll71@gmail.com>
2023-08-13 22:14:09 +0600

    nav section 70% complete

--- src/component/navbar/Bottom.jsx
+++ src/component/navbar/Bottom.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 // import Home from './dropdown/Home'
 // import About from './dropdown/About'
@@ -14,92 +14,127 @@ import NavJson from '../../../public/Nav.json'
 // import Top from './Top'
 
 const Bottom = () => {
-  
+
+  // const [isOpen, setisOpen] = useState(false)
+    const [showSubMenu, setShowSubMenu] = useState([])
+
+
+  const onEnterHandler = (subMenuId) => {
+    setShowSubMenu((prev) => {
+      console.log("running");
+      let arr = [...prev]
+      arr[subMenuId] = true
+      return arr;
+
+    })
+
+  }
+
+   const subMousLeaveHandle = (subMenuId) => {
+    setShowSubMenu((prev)=>{
+              console.log("running");
+              let arr = [...prev]
+              arr[subMenuId] =false
+              return arr;
+
+          })
+
+      }
 
 
 
-    return (
-        <div className='relative'>
-            <div className=' w-full z-50 top-10 absolute'>
-                <div className='flex justify-between mx-auto bg-green-400 p-6 w-[78rem] ' >
-                    <h2 className="bg-green-400 text-2xl"> helo i am bottom</h2>
+
+  return (
+    <div className='relative'>
+      <div className='w-full z-50 top-10 absolute'>
+        <div className='flex justify-between bg-green-400 mx-auto p-6 w-[78rem] ' >
+          <h2 className="text-2xl"> helo i am bottom</h2>
 
 
-                    <div className=" flex   list-none ">
-                        {NavJson.map((item, i) => {
-                            if (!item.children) {
-                                return (
-                                    <li key={item.id} className='mr-5 pb-6 hover:text-white'>
-                                        <a href='#'>
-                                            <span>{item.name}</span>
-                                        </a>
-                                    </li>
-                                )
-                            }
-                            return (
-                                <li
-                                 
-                                    key={item.id}
-                                >
-                                    <div>
-                                        <span>
-
-                                            {item.name}
-                                        </span>
-                                    </div>
-                                    {
-                                        item.children.map((subItem) => {
-                                            return (
-                                                <li key={subItem.id} className='hover:text-white'>
-                                                    <a>
-                                                        <span href='#' className='block'>
-                                                            {subItem.name}
-                                                        </span>
-                                                    </a>
-
-                                                </li>
-                                            )
-                                        })
-                                    }
-
-
-                                </li>
-                            )
-                        })}
-
-
-                        <div className="flex-3  ">
-                            <ul className="flex justify-center  space-x-5">
-                                <li>
-                                    <a href="#" class="text-white hover:text-gray-900  dark:hover:text-white dark:text-gray-400">
-                                        <HiOutlineClipboard className='absolute mt-1 w-6 h-6 border-r-2' /><span className='relative bg-blue-400 ml-3'>0</span>
-
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#" className="text-white hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
-                                        <HiSearch className='mt-1 w-6 h-6 border-r-2' />
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#" className="text-white hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
-                                        <HiMenu className='mt-1 w-6 h-6' />
-                                    </a>
-                                </li>
-
-                            </ul>
-
-
-                        </div>
+          <div className=" flex  list-none ">
+            <ul className='flex '>
+              {NavJson.map((item, i) => {
+                // { console.log(item.name) }
+                if (!item.children) {
+                  return (
+                    <li
+                      key={item.id} className='mr-5 hover:text-white'>
+                      <a href={item.href}>
+                        <span>{item.name}</span>
+                      </a>
+                    </li>
+                  )
+                }
+                return (
+                  <li
+                  onMouseEnter={() => onEnterHandler(item.id)}
+                  onMouseLeave={() => subMousLeaveHandle(item.id)}
+                    key={item.id}
+                    className=''
+                  >
+                    <div className='hover:text-white mr-5 bg-blue-400'>
+                      <span
+                        onclick={() => setisOpen((prev) => !prev)}
+                      >
+                        {item.name}
+                      </span>
                     </div>
 
+                    {
+                      showSubMenu[item.id]&&
+                      item.children.map((Submenu) => {
+                        return (
+                          <li key={Submenu.id} className=' '>
+                            {/* {console.log(Submenu)} */}
+                            <a>
+                              <span href='#' className=''>
+                                {Submenu.name}
+                              </span>
+                            </a>
+                            {/* {console.log(Submenu)} */}
+                          </li>
+                        )
+                      })
+                    }
 
-                    {/*  */}
-                </div>
+                  </li>
+                )
+              })}
+            </ul>
+
+
+            <div className="flex-3  ">
+              <ul className="flex justify-center  space-x-5">
+                <li>
+                  <a href="#" class="text-white hover:text-gray-900  dark:hover:text-white dark:text-gray-400">
+                    <HiOutlineClipboard className='absolute mt-1 w-6 h-6 border-r-2' /><span className='relative bg-blue-400 ml-3'>0</span>
+
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="text-white hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
+                    <HiSearch className='mt-1 w-6 h-6 border-r-2' />
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="text-white hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
+                    <HiMenu className='mt-1 w-6 h-6' />
+                  </a>
+                </li>
+
+              </ul>
+
+
             </div>
-        </div>
+          </div>
 
-    )
+
+          {/*  */}
+        </div>
+      </div>
+    </div>
+
+  )
 }
 
 export default Bottom
--- src/component/navbar/dropdown/About.jsx
+++ src/component/navbar/dropdown/About.jsx
@@ -3,23 +3,23 @@ import list from './list.json'
 import { HiChevronDown } from "react-icons/hi";
 
 const About = () => {
-     const [isOpen ,SetIsOpen] = useState(false)
+  const [isOpen, SetIsOpen] = useState(false)
   return (
     <div className='relative'>
-         
-         <button onClick={()=>SetIsOpen((prev)=>!prev)}className=' flex justify-between hover:text-white'>About{<HiChevronDown className='ml-2 mt-2' />}
-</button>
-         {isOpen && <div className='mt-8 z-10 absolute opacity-60'>
-            {list.map((item,i)=>(
-                <div className=' bg-black text-gray-400 hover:text-white p-1 h-10 w-40'>
-                    <h3 className='mt-2'>{item.manuName}</h3>
-                </div>
-            ))}
-            
-            
-            </div>}
+
+      <button onClick={() => SetIsOpen((prev) => !prev)} className=' flex justify-between hover:text-white'>About{<HiChevronDown className='ml-2 mt-2' />}
+      </button>
+      {isOpen && <div className='mt-8 z-10 absolute opacity-60'>
+        {list.map((item, i) => (
+          <div className=' bg-black text-gray-400 hover:text-white p-1 h-10 w-40'>
+            <h3 className='mt-2'>{item.manuName}</h3>
+          </div>
+        ))}
+
+
+      </div>}
     </div>
-   
+
   )
 }
 
