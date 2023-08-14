a81778250e32cb19a123cad4964fe0fb473b69ac
proshantosaha <proshantoll71@gmail.com>
2023-08-14 17:51:40 +0600

    complete award section

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -3,23 +3,43 @@ import leftDown from '../../assets/about/tickets.jpg'
 import leftUp from '../../assets/about/man-hiking-2.jpg'
 import air from '../../assets/about/f-icon-1.png'
 import calander from '../../assets/about/f-icon-2.png'
+import yellow from '../../assets/about/yellow-3.png'
+import pink from '../../assets/about/pink-4.png'
+
+// import { FiArrowRight } from "react-icons/fi";
+
+
+
+
 
 
 const About = () => {
   return (
     // sm:w-11/12 md:w-10/12 lg:w-9/12
-    <div className="w-full  flex justify-between border-t-2 mt-20">
+    <div className="w-full  flex justify-between border-t-2 ">
 
-      <div className="flex justify-between  mx-auto w-[78rem] ">
+      <div className="flex justify-between  mx-auto w-[78rem] mt-40 h-full ">
         <div className="w-[50rem] ">
+          {/* <div className="mr-20">
+            <img src={yellow} alt="" />
+          </div> */}
           {/* h-auto rounded-lg */}
-          <div className="absolute ">
-            <img src={leftDown} className="w-full rotate-[15deg] ml-[40px] rounded-md" alt="tour" />
+          <div className="absolute flex ">
+            <div>
+            <img src={yellow} alt="" className="w-full animate-spin align-[-0.125em] " />
+            </div>
+           
+            <img src={leftDown} className="w-full rotate-[15deg] rounded-md" alt="tour" />
           </div>
 
-          <div className="relative">
-            <img src={leftUp} className=" mt-[98px] rotate-[-10deg] mt-60 rounded-md border-8" alt="tour" />
+          <div className="relative flex">
+            <img src={leftUp} className=" mt-[98px] rotate-[-10deg] mt-[200px] rounded-md border-8" alt="tour" />
+
+           
           </div>
+          <div className="ml-60">
+              <img src={pink} alt="" />
+            </div>
 
         </div>
 
@@ -59,7 +79,7 @@ const About = () => {
 
           <div className="mt-8">
             <ul className="leading-10">
-              <li>Inspiring travel experiences</li>
+               <li className="">Inspiring travel experiences</li>
               <li>Expertly curated itineraries</li>
               <li>Personalized guidance and support</li>
             </ul>
