c4664bf1a5ca7884be61cc2e299d5c9da142732e
Ikbal101 <islamlover000001@gmail.com>
2023-08-22 02:25:42 +0600

    push

--- src/Pages/Shop/ProductDetails.jsx
+++ src/Pages/Shop/ProductDetails.jsx
@@ -25,7 +25,7 @@ const ProductDetails = () => {
           <div className="lg:w-4/5 mx-auto flex flex-wrap">
             <img
               alt="ecommerce"
-              className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+              className="w-full lg:w-1/2 lg:h-auto h-64 object-cover object-center rounded"
               src={details?.image}
             />
             <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
